3d7f4d266f531b164382050ba1776bea9f09f598
KilliannH <killiannhervagault@gmail.com>
2020-04-30 20:01:26 +0200

    debug

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyparser.json());
 
 const port = process.env.PORT || 3000;
-const db_url = `mongodb://@localhost:27028/first_db`;
+const db_url = `mongodb://root:example@localhost:27028/first_db`;
 
 mongoose.connect(db_url, (err) => {
     if(err) {
