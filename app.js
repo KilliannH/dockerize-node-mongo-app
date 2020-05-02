619bd9af606d12f4d539266c13948ab715c27e36
KilliannH <killiannhervagault@gmail.com>
2020-05-02 23:11:40 +0200

    trying to add user, password to access mongodb

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyparser.json());
 const port = PORT || 3000;
 const host = HOST || "localhost";
 
-const db_url = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const db_url = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=${DB_USER}`;
 
 mongoose.connect(db_url, (err) => {
     if(err) {
