9185fbab8cffd117bb7be520adff28a3ce6b95dd
KilliannH <killiannhervagault@gmail.com>
2020-05-02 23:47:33 +0200

    trying to add user, password to access mongodb

---
+++ mongo-init.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "test-user",
+        pwd: "test-password",
+        roles: [
+            {
+                role: "readWrite",
+                db: "test-db"
+            }
+        ]
+    }
+);
