3fb5af26826fa776b10da8d259ee30136da35df0
KilliannH <killiannhervagault@gmail.com>
2022-07-10 18:04:33 +0200

    some updates + no more youtube-dl

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyparser = require('body-parser');
 
 const {
-    API_KEY,
     HOST,
     PORT,
     DB_HOST,
@@ -22,16 +21,35 @@ const host = HOST || "localhost";
 
 const db_url = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
-mongoose.connect(db_url, (err) => {
-    if(err) {
-        console.error(err);
-    } else {
-        console.log("Database connection established successfully");
-    }
+mongoose.connect(db_url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}, () => {
+  console.log("Db connected...");
 });
+
+var yogourtSchema = mongoose.Schema({
+    yogourt: {
+        type: String,
+        required: true
+    },
+});
+
+mongoose.model('Yogourts', yogourtSchema);
 
 app.get('/', (req, res) => {
     res.send("container works");
+});
+
+app.post('/yogourt', (req, res) => {
+	const yogourt = req.body;
+	Yogourt.create(yogourt, (err, cb) => {
+        if(err) {
+            res.status(500).json({success: false, message: "Internal Server Error"});
+        } else {
+            res.json({success: true, vocab: vocab});
+        }
+    });
 });
 
 app.listen(port, host, () => console.log(`App running successfully on port number ${port}...`));
